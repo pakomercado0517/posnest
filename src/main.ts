8adfc7447ec9329dfaa12bba41802468a6cb911c
pakomercado0517 <sissyyayle0517@hotmail.com>
2025-06-01 21:44:34 -0600

    fixing crypto connection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-require-imports */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+(global as any).crypto = require('crypto');
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
